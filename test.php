bfe0224bbf8d13ab16ecb7ab96e97f17bab19369
r4mzz <49102720+r4mzz@users.noreply.github.com>
2019-07-21 01:44:11 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,20 @@
+print"
+
+               +RRRRRR        44+   +M     M++  xxxxxxxxxx             
+               +Md  md-     'sMM+   +MM+  sM++  hhhmMm-xx              
+               +Mo  oM/    -dd M+   +MNN-/NN++      -mm.               
+               +MhsNN+    /Nd  M+   +M hmNi M+     /Nh`                
+               +M/ -Nh.   +ooodM+   +M  mo  h+    +NNsss+              
+               +X   XX.        X+   +M      M+   xxxxxxxxx             
+                                                                      
+                                                                      
+    +o. -oo  /o.` :oss+.  ooooo+. yoooo+-  oo      +o`  :oys::  sooooo  
+    :Ms yMM:`md` hN+-:hN: Md:::Nh yM/::hN: mm      yM` -Mh      ::dN::  
+     yN+MomdsM- -Ms   .My MNyhdNo yM`  .My mm      yM` `yddddo    hN      
+     .NMd :MMs  `mm:`.sM+ Mh./Nh  yM-..oM+ mm.... `yM`     -dM`   hN      
+      oh:  yh.   `oyhhy:  hs  /h+ yohyyyy  yhyyyy  oh` :;yhhy/    sh     
+
+
+
+
+";
